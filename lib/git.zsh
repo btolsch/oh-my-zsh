372d9b735c08e92292bf6e01182b55e40f183e24
btolsch <btolsch@gmail.com>
2014-07-02 16:23:41 -0600

    Changed prompt git-awareness and added my theme.

--- lib/git.zsh
+++ lib/git.zsh
@@ -18,7 +18,18 @@ parse_git_dirty() {
       FLAGS+='--ignore-submodules=dirty'
     fi
     if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" == "true" ]]; then
-      FLAGS+='--untracked-files=no'
+        GIT_STATUS=$(command git status -s ${SUBMODULE_SYNTAX} -uno 2> /dev/null)
+    else
+        GIT_STATUS=$(command git status -s ${SUBMODULE_SYNTAX} 2> /dev/null)
+    fi
+    if [[ -n $GIT_STATUS ]]; then
+      if echo "$GIT_STATUS" | grep -E "^.[^ ]" &>/dev/null; then
+        echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+      else
+        echo "$ZSH_THEME_GIT_PROMPT_CACHED"
+      fi
+    else
+      echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
     fi
     STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
   fi
